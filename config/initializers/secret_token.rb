5ce5df50bc791d26de59776a027d82961b3b2fd6
Jorge Rivera <jdrivera@memphis.edu>
2014-10-20 15:07:31 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CompApp::Application.config.secret_key_base = 'e1b1a94886fd536cab6c187c64429819a1b16990412733e8db5b1d09db279aa78fa895457a8c022973a4b574607816b5e8aa565ef1c3090298ca3e9d3efb9d32'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CompApp::Application.config.session_store :cookie_store, key: '_comp_app_session'
