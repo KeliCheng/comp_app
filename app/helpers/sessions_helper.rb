5c75d9f05401fc829ca9aecaf56540699810bcc5
Shannon Kelly <smkelly3@memphis.edu>
2014-10-21 19:11:39 -0500

    commented out additions: if/else for what to display on home page, signed_in variable in sessions_helper

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,14 @@
+# # Uncomment when user db has been estbalished
+
+# module SessionHelper
+
+# 	def sign_in(user)
+# 		cookies.permanent[:remember_token] = user.remember_token
+# 		self.current_user = user
+# 	end
+
+# 	def signed_in?
+# 		!current_user.nil?
+# 	end
+
+# end
