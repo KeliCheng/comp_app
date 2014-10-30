58c9eaf35d33c5898e5e8d515fe94d3ee7a0f30c
Shannon Kelly <smkelly3@memphis.edu>
2014-10-30 15:55:12 -0500

    Finished edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,43 @@
 class UsersController < ApplicationController
 
-	def edit
+	def edit  
+    @user = User.find(params[:id])  
 	end
 
 	def new
-        @user = User.new
+    @user = User.new
   end
 
-	def create
-  @user = User.new(user_params)
-    	if @user.save
+  def create
+    @user = User.new(user_params)
+    if @user.save
         #sign_in @user
         flash[:success] = "Welcome to the Sample App!"
-    		redirect_to @user
-    	else
-    		render 'new'
-    	end
-	end
+        redirect_to @user
+      else
+        render 'new'
+      end
+    end
 
-def show
-  	@user = User.find(params[:id])
-end
+    def show
+     @user = User.find(params[:id])
+   end
 
-private
+   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
-		def user_params
-			params.require(:user).permit(:name, :email, :password, :password_confirmation)
-		end
+  private
+
+  def user_params
+   params.require(:user).permit(:name, :email, :password, :password_confirmation)
+ end
+
 
 end
