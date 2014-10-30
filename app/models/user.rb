0019dd7e635f3f45a9d55c340e916197ade4f69a
Jorge Rivera <mango01010101@gmail.com>
2014-10-30 12:08:27 -0500

    Now validates email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 		validates :password_confirmation, :presence => true
 		validates :name, presence: true, uniqueness: true
 		validates :email, presence: true, uniqueness: true
-
+		validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 	# Returns a random token.
   def User.new_token
     SecureRandom.urlsafe_base64
