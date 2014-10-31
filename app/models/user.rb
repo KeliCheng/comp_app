19fc44bb3db9c950fc651ff34b3d85e1798729eb
Tyler Brown <tylerbrown47@gmail.com>
2014-10-31 12:50:28 -0500

    force email to lowercase

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 		validates :email, presence: true, uniqueness: true
 		validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
     has_secure_password
+    before_save {|user| user.email.downcase!}
 	# Returns a random token.
 
   # Returns the hash digest of the given string.
