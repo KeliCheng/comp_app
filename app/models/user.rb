eca07e92037a4b754853d05c1d4bc4123640af0c
Bobby Richmond <rjrchmnd@memphis.edu>
2014-10-31 12:30:31 -0500

    Login Betav2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,16 @@ class User < ActiveRecord::Base
 		validates :name, presence: true, uniqueness: true
 		validates :email, presence: true, uniqueness: true
 		validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    has_secure_password
 	# Returns a random token.
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
   def User.new_token
     SecureRandom.urlsafe_base64
   end
