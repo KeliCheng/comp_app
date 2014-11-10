dc1bdd3b1bf876a796fac0b3fd33367ed50a02d5
Keli Cheng <kcheng@memphis.edu>
2014-11-10 16:35:34 -0600

    modified ensure compad

--- app/controllers/component_pages_controller.rb
+++ app/controllers/component_pages_controller.rb
@@ -20,23 +20,26 @@ class ComponentPagesController < ApplicationController
 
   def cpu
     init_prices
-    @cpu = Cpu.all
+    #@cpu = Cpu.all
     @cpu = Cpu.where(mb_compad: Motherboard.find_by(id: @computer.motherboard_id).cpu_compad)
   end
 
   def rvm
     init_prices
-    @rvm = Ram.all
+    #@rvm = Ram.all
+    @rvm = Rvm.where(mb_compad: Motherboard.find_by(id: @computer.motherboard_id).ram_compad)
   end
 
   def gpu
     init_prices
-    @gpu = Gpu.all
+    #@gpu = Gpu.all
+    @gpu = Gpu.where(mb_compad: Motherboard.find_by(id: @computer.motherboard_id).gpu_compad)
   end
 
   def hd
     init_prices
-    @hd = Hd.all
+    #@hd = Hd.all
+    @hd = Hd.where(mb_compad: Motherboard.find_by(id: @computer.motherboard_id).hd_compad)
   end
 
 
